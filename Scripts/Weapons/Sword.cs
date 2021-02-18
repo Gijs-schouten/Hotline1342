2d25f9d5616b435108fe42787ca55f2a5ed39be2
Gijs Schouten <gijswow@gmail.com>
2021-02-18 11:54:58 +0100

    weapon progress

---
+++ Scripts/Weapons/Sword.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject.Scripts.Weapons
+{
+    class Sword
+    {
+    }
+}
---
+++ Scripts/Weapons/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseProject.Scripts.Weapons
+{
+    class Weapon
+    {
+        public float WeaponDamage { get; set; }
+        public float WeaponSpeed { get; set; }
+        public SpriteBatch sprite { get; set; }
+
+        public event Action<float> HitsObject;
+
+        private void Collide() 
+        {
+            HitsObject(WeaponDamage);
+        }
+       
+    }
+}
