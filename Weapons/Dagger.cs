cef14f7fb677dd5dfe6c354e2d2ffc20a2ea1a3e
Antonio Bottelier <Antonio3338@gmail.com>
2021-03-12 02:00:16 +0100

    Moved the location of the weapons and fixed the namespaces

--- Weapons/Dagger.cs
+++ Weapons/Dagger.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace PadZex.Scripts.Weapons
+namespace PadZex.Weapons
 {
     class Dagger : Weapon
     {
--- Weapons/Sword.cs
+++ Weapons/Sword.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace PadZex.Scripts.Weapons
+namespace PadZex.Weapons
 {
     public class Sword : Weapon
     {
