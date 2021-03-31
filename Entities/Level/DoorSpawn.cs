900800641b5e5d0524dcbd3cb8086277228deed5
Antonio Bottelier <Antonio3338@gmail.com>
2021-03-31 17:26:14 +0200

    Added a few base entities for spawning in the map

---
+++ Entities/Level/DoorSpawn.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PadZex.Entities.Level
+{
+    public class DoorSpawn : Entity
+    {
+        public override void Draw(SpriteBatch spriteBatch, Time time)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Time time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
