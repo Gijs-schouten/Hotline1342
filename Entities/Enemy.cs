5d7eff7f6ff9e580ddd13266ec4e5ca0db22b747
Antonio Bottelier <Antonio3338@gmail.com>
2021-05-10 12:33:34 +0200

    Fixed damage when player touches enemy

--- Entities/Enemy.cs
+++ Entities/Enemy.cs
@@ -78,7 +78,7 @@ namespace PadZex
 
         public override Shape CreateShape()
         {
-            var shape = new Collision.Rectangle(this, Vector2.Zero, new Vector2(enemySprite.Width, enemySprite.Height));
+            var shape = new Collision.Rectangle(this, new Vector2(-(float)enemySprite.Width/2*Scale, -(float)enemySprite.Height/2*Scale), new Vector2(enemySprite.Width, enemySprite.Height));
             return shape;
         }
 
