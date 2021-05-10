3c16ec85d2793c4efcb6b8a456ed81afe67b74fe
Antonio Bottelier <Antonio3338@gmail.com>
2021-05-10 13:18:13 +0200

    added a pause frame to the hitstun so the game continues a little bit before freezing again

--- HitStun.cs
+++ HitStun.cs
@@ -7,14 +7,17 @@ namespace PadZex
         /// <summary>
         /// How long is a single frame?
         /// </summary>
-        private const float FRAME_DURATION = 0.16f;
+        private const float FRAME_DURATION = 0.16f / 8;
+
+        private const float PAUSE_FRAME_TIME = 0.16f / 4;
 
         private static int hitStunAmount = 0;
         private static float currentHitStunTime = FRAME_DURATION;
+        private static bool isPauseFrame = false;
 
         public static void Add(int time)
         {
-            if (time < 1) throw new ArgumentOutOfRangeException(nameof(time));
+            if (time < 1) return;
             hitStunAmount += time;
         }
 
@@ -27,12 +30,24 @@ namespace PadZex
         {
             if (hitStunAmount == 0) return false;
 
+            if (isPauseFrame)
+            {
+                currentHitStunTime -= deltaTime;
+                if (currentHitStunTime <= 0)
+                {
+                    currentHitStunTime = FRAME_DURATION;
+                    isPauseFrame = false;
+                }
+                return true;
+            }
+
             currentHitStunTime -= deltaTime;
             
             if (currentHitStunTime <= 0)
             {
                 hitStunAmount--;
-                currentHitStunTime = FRAME_DURATION;
+                isPauseFrame = true;
+                currentHitStunTime = PAUSE_FRAME_TIME;
                 return false;
             }
 
